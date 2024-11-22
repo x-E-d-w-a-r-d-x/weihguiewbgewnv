c1751b299d6f7833b0e069f7e91d1a932e67e7dd
Derek Lucier <derekjlucier@gmail.com>
2024-11-22 16:30:56 -0500

    Polished the website with animations, responsiveness, and a back-to-top button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 const gamesList = document.querySelector('.games-list');
+const backToTop = document.getElementById('back-to-top');
 
 // List of games
 const games = [
     {
-        title: 'Game 1',
-        description: 'A fun platformer game.',
-        thumbnail: 'games/game1/thumbnail.jpg', // Add thumbnails to each game's folder
-        link: 'games/game1/index.html'
-    },
-    {
-        title: 'Game 2',
-        description: 'Test your reflexes in this action-packed game.',
-        thumbnail: 'games/game2/thumbnail.jpg',
-        link: 'games/game2/index.html'
+        title: 'Slope',
+        description: 'Control the ball down the slope for as long as posible.',
+        thumbnail: 'games/Slope/slope4.jpeg', // Add thumbnails to each game's folder
+        link: 'games/Slope/index.html'
     }
 ];
 
@@ -21,10 +16,24 @@ games.forEach(game => {
     const gameItem = document.createElement('div');
     gameItem.classList.add('game-item');
     gameItem.innerHTML = `
-        <img src="${game.thumbnail}" alt="${game.title} Thumbnail">
+        <img src="${game.thumbnail}" alt="${game.title} Thumbnail" loading="lazy">
         <h3>${game.title}</h3>
         <p>${game.description}</p>
         <a href="${game.link}" target="_blank">Play Now</a>
     `;
     gamesList.appendChild(gameItem);
 });
+
+// Show button when scrolling
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 200) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none';
+    }
+};
+
+// Smooth scroll to top
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
